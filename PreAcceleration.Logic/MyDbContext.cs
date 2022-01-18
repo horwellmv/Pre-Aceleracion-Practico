6a442df0646dd06cc823c339d131e5143dbcdc13
Horwell <horwellmv@gmail.com>
2022-01-18 16:37:00 -0300

    Actualizacion de proyecto, organizado en capa de entidades y capa logica. Agregamos documentacion de codigo.

---
+++ PreAcceleration.Logic/MyDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using PreAcceleration.Entities;
+
+
+namespace PreAcceleration.UI
+{
+    internal class MyDbContext : DbContext
+    {
+        //Configuracion inicial Opcional de nombre, servidor y tipo de logeo en la Base de datos.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.; Database=DbPreAcceleration;Trusted_Connection=True;");
+        }
+
+        //Mis Entidades
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comments> Comments { get; set; }
+
+    }
+
+}
+
+// Debemos tener Instalados los Nugets 
+// Microsoft.EntityFrameworkCore
+// Microsoft.EntityFrameworkCore.Design
+// Microsoft.EntityFrameworkCore.SqlServer
+// Microsoft.EntityFrameworkCore.Tools
+
+// Ahora en Tools/Nuget Package Manager/Package Manager Console/ Escribimos los siguientes comandos
+// Add-Migration Initial
+// Update-Database
+// Ahora nuestra Db estará creada con nuestras clases como tablas.
