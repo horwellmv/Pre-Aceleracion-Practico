3e895f19f831c93f9974e673167f40c2f115561d
Horwell <horwellmv@gmail.com>
2022-01-20 01:39:55 -0300

    Cambios en todo el proyecto, ahora usando VS2019, con plantilla .Net Core Web API

---
+++ Entities/Comments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PreAcceleration.HorwellMurillo.Entities
+{
+    public class Comments
+    {
+        //Propiedades escalares de nuestras tablas/entidades.
+        [Key]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Comment { get; set; }
+        public int UserId { get; set; }
+
+        //Propiedades de navegacion (reference) por ser muchos Post por un usuario
+        public User User { get; set; }
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PreAcceleration.HorwellMurillo.Entities
+{
+    public class User
+    {
+        //Propiedades escalares de nuestras tablas/entidades.
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        //Propiedades de navegacion (Collection) Por ser uno a muchos o muchos a muchos.
+        public ICollection<Post> Posts { get; set; }
+        public ICollection<Comments> Comments { get; set; }
+    }
+}
